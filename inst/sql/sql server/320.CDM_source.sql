9e9e33548afab16c5c89ef3387488d34104429e5
Jimyung6642 <cory66421@gmail.com>
2019-02-07 15:02:28 +0900

    Insert cdm_release_date as date format

--- inst/sql/sql server/320.CDM_source.sql
+++ inst/sql/sql server/320.CDM_source.sql
@@ -14,4 +14,4 @@ Insert into @NHISNSC_database.CDM_SOURCE
 			 cdm_release_date, cdm_version, vocabulary_version)
 values('The National Health Insurance Service–National Sample Cohort', 'NHIS-NSC', 'The National Health Insurance Service in South Korea',
 		'A representative sample cohort of 1,025,340 participants was randomly selected, comprising 2.2% of the total eligible Korean population in 2002, and followed for 11 years until 2013 unless participants’ eligibility was disqualified due to death or emigration.',
-		'http://nhiss.nhis.or.kr/bd/ab/bdaba021eng.do', 'https://github.com/OHDSI/ETL---Korean-NSC', 'Jul 20, 2017', 'v5.3.1', 'v5.0 04 Dec 18')
+		'http://nhiss.nhis.or.kr/bd/ab/bdaba021eng.do', 'https://github.com/OHDSI/ETL---Korean-NSC', '2017-07-20', 'v5.3.1', 'v5.0 04 Dec 18')
