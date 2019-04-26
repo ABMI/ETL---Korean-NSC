064788c8a635904548dab37e88922459c1a69630
Jimyung6642 <cory66421@gmail.com>
2019-04-26 17:40:05 +0900

    Done with procedure and cost, currently working on dose_era

--- inst/sql/sql server/110.Procedure_occurrence.sql
+++ inst/sql/sql server/110.Procedure_occurrence.sql
@@ -189,67 +189,6 @@ FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd,
 WHERE left(a.div_cd,5)=b.source_code
 ;
 
-/**************************************
- 3-3. Insert 30T data which are unmapped with temp mapping table 
-***************************************/
-INSERT INTO @NHISNSC_database.PROCEDURE_OCCURRENCE 
-	(procedure_occurrence_id, person_id, procedure_concept_id, procedure_date, procedure_type_concept_id, 
-	modifier_concept_id, quantity, provider_id, visit_occurrence_id, procedure_source_value, 
-	procedure_source_concept_id)
-SELECT
-	convert(bigint, convert(bigint, a.master_seq)*10 + convert(bigint, row_number() over (partition by a.key_seq, a.seq_no order by a.div_cd))) as procedure_occurrence_id,
-	a.person_id as person_id,
-	0 as procedure_concept_id,
-	CONVERT(VARCHAR, a.recu_fr_dt, 112) as procedure_date,
-	45756900 as procedure_type_concept_id,
-	NULL as modifier_concept_id,
-	convert(float, a.dd_mqty_exec_freq) * convert(float, a.mdcn_exec_freq) * convert(float, a.dd_mqty_freq) as quantity,
-	NULL as provider_id,
-	a.key_seq as visit_occurrence_id,
-	a.div_cd as procedure_source_value,
-	null as procedure_source_concept_id
-FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd, 
-			case when x.mdcn_exec_freq is not null and isnumeric(x.mdcn_exec_freq)=1 and cast(x.mdcn_exec_freq as float) > '0' then cast(x.mdcn_exec_freq as float) else 1 end as mdcn_exec_freq,
-			case when x.dd_mqty_exec_freq is not null and isnumeric(x.dd_mqty_exec_freq)=1 and cast(x.dd_mqty_exec_freq as float) > '0' then cast(x.dd_mqty_exec_freq as float) else 1 end as dd_mqty_exec_freq,
-			case when x.dd_mqty_freq is not null and isnumeric(x.dd_mqty_freq)=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
-			y.master_seq, y.person_id
-	FROM (select * from @NHISNSC_rawdata.@NHIS_30T where div_type_cd in ('1', '2')) x, 
-		 (select master_seq, key_seq, seq_no, person_id from @NHISNSC_database.SEQ_MASTER where source_table='130') y
-	WHERE x.key_seq=y.key_seq
-	AND x.seq_no=y.seq_no) a 
-WHERE left(a.div_cd,5) not in (select source_code from #mapping_table )
-;
-
-/**************************************
- 3-4. Insert 60T data which are unmapped with temp mapping table 
-***************************************/
-INSERT INTO @NHISNSC_database.PROCEDURE_OCCURRENCE 
-	(procedure_occurrence_id, person_id, procedure_concept_id, procedure_date, procedure_type_concept_id, 
-	modifier_concept_id, quantity, provider_id, visit_occurrence_id, procedure_source_value, 
-	procedure_source_concept_id)
-SELECT 
-	convert(bigint, convert(bigint, a.master_seq)*10 + convert(bigint, row_number() over (partition by a.key_seq, a.seq_no order by a.div_cd))) as procedure_occurrence_id,
-	a.person_id as person_id,
-	0 as procedure_concept_id,
-	CONVERT(VARCHAR, a.recu_fr_dt, 112) as procedure_date,
-	45756900 as procedure_type_concept_id,
-	NULL as modifier_concept_id,
-	convert(float, a.dd_mqty_freq) * convert(float, a.dd_exec_freq) * convert(float, a.mdcn_exec_freq) as quantity,
-	NULL as provider_id,
-	a.key_seq as visit_occurrence_id,
-	a.div_cd as procedure_source_value,
-	null as procedure_source_concept_id
-FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd, 
-			case when x.mdcn_exec_freq is not null and isnumeric(x.mdcn_exec_freq)=1 and cast(x.mdcn_exec_freq as float) > '0' then cast(x.mdcn_exec_freq as float) else 1 end as mdcn_exec_freq,
-			case when x.dd_exec_freq is not null and isnumeric(x.dd_exec_freq)=1 and cast(x.dd_exec_freq as float) > '0' then cast(x.dd_exec_freq as float) else 1 end as dd_exec_freq,
-			case when x.dd_mqty_freq is not null and isnumeric(x.dd_mqty_freq)=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
-			y.master_seq, y.person_id
-	FROM (select * from @NHISNSC_rawdata.@NHIS_60T where div_type_cd in ('1', '2')) x, 
-		 (select master_seq, key_seq, seq_no, person_id from @NHISNSC_database.SEQ_MASTER where source_table='160') y
-	WHERE x.key_seq=y.key_seq
-	AND x.seq_no=y.seq_no) a 
-WHERE left(a.div_cd,5) not in (select source_code from #mapping_table)
-;
 
 /**************************************
  3-5. Insert data using 30T duplicated
@@ -312,5 +251,70 @@ FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd,
 	AND x.seq_no=y.seq_no) a, #duplicated b
 WHERE left(a.div_cd,5)=b.source_code
 ;
+
+
+/**************************************
+ 3-3. Insert 30T data which are unmapped with temp mapping table 
+***************************************/
+INSERT INTO @NHISNSC_database.PROCEDURE_OCCURRENCE 
+	(procedure_occurrence_id, person_id, procedure_concept_id, procedure_date, procedure_type_concept_id, 
+	modifier_concept_id, quantity, provider_id, visit_occurrence_id, procedure_source_value, 
+	procedure_source_concept_id)
+SELECT
+	convert(bigint, convert(bigint, a.master_seq)*10 + convert(bigint, row_number() over (partition by a.key_seq, a.seq_no order by a.div_cd))) as procedure_occurrence_id,
+	a.person_id as person_id,
+	'0' as procedure_concept_id,
+	CONVERT(VARCHAR, a.recu_fr_dt, 112) as procedure_date,
+	45756900 as procedure_type_concept_id,
+	NULL as modifier_concept_id,
+	convert(float, a.dd_mqty_exec_freq) * convert(float, a.mdcn_exec_freq) * convert(float, a.dd_mqty_freq) as quantity,
+	NULL as provider_id,
+	a.key_seq as visit_occurrence_id,
+	a.div_cd as procedure_source_value,
+	null as procedure_source_concept_id
+FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd, 
+			case when x.mdcn_exec_freq is not null and isnumeric(x.mdcn_exec_freq)=1 and cast(x.mdcn_exec_freq as float) > '0' then cast(x.mdcn_exec_freq as float) else 1 end as mdcn_exec_freq,
+			case when x.dd_mqty_exec_freq is not null and isnumeric(x.dd_mqty_exec_freq)=1 and cast(x.dd_mqty_exec_freq as float) > '0' then cast(x.dd_mqty_exec_freq as float) else 1 end as dd_mqty_exec_freq,
+			case when x.dd_mqty_freq is not null and isnumeric(x.dd_mqty_freq)=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
+			y.master_seq, y.person_id
+	FROM (select * from @NHISNSC_rawdata.@NHIS_30T where div_type_cd in ('1', '2')) x, 
+		 (select master_seq, key_seq, seq_no, person_id from @NHISNSC_database.SEQ_MASTER where source_table='130') y
+	WHERE x.key_seq=y.key_seq
+	AND x.seq_no=y.seq_no) a 
+WHERE left(a.div_cd,5) not in (select source_code from #duplicated union all select source_code from #mapping_table )
+;
+
+/**************************************
+ 3-4. Insert 60T data which are unmapped with temp mapping table 
+***************************************/
+INSERT INTO @NHISNSC_database.PROCEDURE_OCCURRENCE 
+	(procedure_occurrence_id, person_id, procedure_concept_id, procedure_date, procedure_type_concept_id, 
+	modifier_concept_id, quantity, provider_id, visit_occurrence_id, procedure_source_value, 
+	procedure_source_concept_id)
+SELECT 
+	convert(bigint, convert(bigint, a.master_seq)*10 + convert(bigint, row_number() over (partition by a.key_seq, a.seq_no order by a.div_cd))) as procedure_occurrence_id,
+	a.person_id as person_id,
+	'0' as procedure_concept_id,
+	CONVERT(VARCHAR, a.recu_fr_dt, 112) as procedure_date,
+	45756900 as procedure_type_concept_id,
+	NULL as modifier_concept_id,
+	convert(float, a.dd_mqty_freq) * convert(float, a.dd_exec_freq) * convert(float, a.mdcn_exec_freq) as quantity,
+	NULL as provider_id,
+	a.key_seq as visit_occurrence_id,
+	a.div_cd as procedure_source_value,
+	null as procedure_source_concept_id
+FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd, 
+			case when x.mdcn_exec_freq is not null and isnumeric(x.mdcn_exec_freq)=1 and cast(x.mdcn_exec_freq as float) > '0' then cast(x.mdcn_exec_freq as float) else 1 end as mdcn_exec_freq,
+			case when x.dd_exec_freq is not null and isnumeric(x.dd_exec_freq)=1 and cast(x.dd_exec_freq as float) > '0' then cast(x.dd_exec_freq as float) else 1 end as dd_exec_freq,
+			case when x.dd_mqty_freq is not null and isnumeric(x.dd_mqty_freq)=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
+			y.master_seq, y.person_id
+	FROM (select * from @NHISNSC_rawdata.@NHIS_60T where div_type_cd in ('1', '2')) x, 
+		 (select master_seq, key_seq, seq_no, person_id from @NHISNSC_database.SEQ_MASTER where source_table='160') y
+	WHERE x.key_seq=y.key_seq
+	AND x.seq_no=y.seq_no) a 
+WHERE left(a.div_cd,5) not in (select source_code from #duplicated union all select source_code from #mapping_table)
+;
+
+
 
 drop table #mapping_table, #duplicated;
--- inst/sql/sql server/150.Cost.sql
+++ inst/sql/sql server/150.Cost.sql
@@ -53,6 +53,9 @@ CREATE TABLE @NHISNSC_database.COST (
 /**************************************
  1-1. Using temp mapping table
 ***************************************/ 
+IF OBJECT_ID('tempdb..#mapping_table', 'U') IS NOT NULL
+	DROP TABLE #mapping_table;
+
 select a.source_code, a.target_concept_id, a.domain_id, REPLACE(a.invalid_reason, '', NULL) as invalid_reason 
 into #mapping_table
 from @Mapping_database.source_to_concept_map a join @Mapping_database.CONCEPT b on a.target_concept_id=b.concept_id
